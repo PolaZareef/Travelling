34a0f6feb58c5cae8483569318a1f8a2b7adb9e5
PolaZareef <Polazareef65@gmail.com>
2022-03-22 15:14:09 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Booking from './Components/BookTour/Booktour';
 import Contact from './Components/contact/contact';
@@ -30,36 +29,3 @@ function App() {
   );
 }
 export default App;
-=======
-import './App.css';
-import Booking from './Components/BookTour/Booktour';
-import Contact from './Components/contact/contact';
-import Discount from './Components/Discount/Discount';
-import Dicsover from './Components/Discover/Discover';
-import Gallery from './Components/gallery/gallery';
-import Header from './Components/Header/Header';
-import HotTours from './Components/HotTours/HotTours';
-import MissPeople from './Components/MissionPeople/MissPeople';
-import PeopleSay from './Components/PeopleSay/PeopleSay';
-import Services from './Components/Services/Services';
-import TopPage from './Components/TopPageImages/TopPage';
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <TopPage />
-      <Dicsover />
-      <Services />
-      <HotTours />
-      <MissPeople />
-      <Booking />
-      <PeopleSay />
-      <Discount />
-      <Gallery />
-      <Contact />
-    </div>
-  );
-}
-export default App;
->>>>>>> f588f4874c583f7fef74226b5fc8da7c509c7262
