93291e38585b11203ee1dc6f940aaf6fdd6d6b8f
PolaZareef <Polazareef65@gmail.com>
2022-03-22 16:30:45 +0200

    edit some css

--- src/Components/MissionPeople/MissPeople.js
+++ src/Components/MissionPeople/MissPeople.js
@@ -17,26 +17,26 @@ const MissPeople =()=>{
                 <div className={`col-sm-3 ${classes.coll}`}>
                     <img src={fir} alt='First Person' className={classes.imag} />
                     <h3 className={classes.hh3}>Diana Robinson</h3>
-                    <p className={classes.pp}>Founder, Owner</p>
-                    <h3 className={classes.hh3}>+1 323-923-4683</h3>
+                    <p className={classes.pp}>Founder</p>
+                    <h3 className={classes.hh3}>+323-923-4683</h3>
                 </div>
                 <div className={`col-sm-3  ${classes.coll}`}>
                     <img src={sec} alt='Second Person' className={classes.imag}/>
                     <h3 className={classes.hh3}>Peter McMillan</h3>
                     <p className={classes.pp}>Travel Agent</p>
-                    <h3 className={classes.hh3}>+1 323-923-4683</h3>
+                    <h3 className={classes.hh3}>+323-923-4683</h3>
                 </div>
                 <div className={`col-sm-3  ${classes.coll}`}>
                     <img src={third} alt='Third Person' className={classes.imag}/>
                     <h3 className={classes.hh3}>Jill Peterson</h3>
                     <p className={classes.pp}>Tour Consultant</p>
-                    <h3 className={classes.hh3}>+1 323-933-4788</h3>
+                    <h3 className={classes.hh3}>+323-933-4788</h3>
                 </div>
                 <div className={`col-sm-3  ${classes.coll}`}>
                     <img src={four} alt='Fourth Person' className={classes.imag}/>
                     <h3 className={classes.hh3}>James Smith</h3>
                     <p className={classes.pp}>PR Manager</p>
-                    <h3 className={classes.hh3}>+1 323-943-4698</h3>
+                    <h3 className={classes.hh3}>+323-943-4698</h3>
                 </div>
             </div>
             </Slide>
