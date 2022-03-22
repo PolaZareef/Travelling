c7c8814802eeeb76113b4abb82daa23cfb31f822
PolaZareef <98190960+PolaZareef@users.noreply.github.com>
2022-03-22 13:16:22 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Booking from './Components/BookTour/Booktour';
+import Contact from './Components/contact/contact';
+import Discount from './Components/Discount/Discount';
+import Dicsover from './Components/Discover/Discover';
+import Gallery from './Components/gallery/gallery';
+import Header from './Components/Header/Header';
+import HotTours from './Components/HotTours/HotTours';
+import MissPeople from './Components/MissionPeople/MissPeople';
+import PeopleSay from './Components/PeopleSay/PeopleSay';
+import Services from './Components/Services/Services';
+import TopPage from './Components/TopPageImages/TopPage';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TopPage />
+      <Dicsover />
+      <Services />
+      <HotTours />
+      <MissPeople />
+      <Booking />
+      <PeopleSay />
+      <Discount />
+      <Gallery />
+      <Contact />
+    </div>
+  );
+}
+export default App;
